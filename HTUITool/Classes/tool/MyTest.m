d6dca432cd77763c7e8c769f8d68e4fa0f9e0827
zhanghaitao <zhanghaitao@baoyinxiaofei.com>
2019-01-04 14:42:20 +0800

    0.2.5

--- HTUITool/Classes/tool/MyTest.m
+++ HTUITool/Classes/tool/MyTest.m
@@ -11,13 +11,13 @@
 +(UIImage *)test{
     NSBundle *currentBundle = [NSBundle bundleForClass:[self class]];
     //图片名称要写全称
-    NSString *patch = [currentBundle pathForResource:@"user@2x.png" ofType:nil inDirectory:@"BMCCommon.bundle"];
+    NSString *patch = [currentBundle pathForResource:@"user@2x.png" ofType:nil inDirectory:@"HTUITool.bundle"];
     UIImage *myimage = [UIImage imageWithContentsOfFile:patch];
     
     //    UIStoryboard *board = [UIStoryboard storyboardWithName:@"aa" bundle:[NSBundle bundleForClass:[self class]]];
     
     CGFloat myscale =[UIScreen mainScreen].scale;
-    NSString *str =[NSString stringWithFormat:@"BMCCommon.bundle/user@%.0fx.png",myscale];
+    NSString *str =[NSString stringWithFormat:@"HTUITool.bundle/user@%.0fx.png",myscale];
     UIImage *image = [UIImage imageNamed:str];
     
     
